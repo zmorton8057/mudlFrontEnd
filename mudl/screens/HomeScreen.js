daf788fcba61d9a98ab48d1e089f5fd93b617c6c
Zac Morton <zmorton8057@gmail.com>
2019-10-16 20:52:40 -0700

    tree navigation working

--- mudl/screens/HomeScreen.js
+++ mudl/screens/HomeScreen.js
@@ -23,13 +23,14 @@ class HomeScreen extends Component {
     this.state = {
       moods: [],
       zac: 'zac',
-      level: 'primary_emotion'
+      level: 'primary_emotion',
+      primary: ''
     }
   }
-  updateMoods(primary_emotion,secondary_emotion){
-    let e1=primary_emotion||null;
-    let e2=secondary_emotion||null;
-    API.getMoods(e1,e2)
+  updateMoods(primary_emotion, secondary_emotion) {
+    let e1 = primary_emotion || null;
+    let e2 = secondary_emotion || null;
+    API.getMoods(e1, e2)
       .then(resp => {
         this.setState({ moods: resp.data })
         console.log("log during component did mount")
@@ -42,35 +43,37 @@ class HomeScreen extends Component {
   componentDidMount() {
     this.updateMoods()
   }
-  handlePress(e, primary_emotion, secondary_emotion){
-    if(primary_emotion && secondary_emotion){
-      this.updateMoods(primary_emotion,secondary_emotion)
-    }else if(primary_emotion){
+  handlePress(e, primary_emotion, secondary_emotion) {
+    if (primary_emotion && secondary_emotion) {
+      this.updateMoods(primary_emotion, secondary_emotion)
+    } else if (primary_emotion) {
       this.updateMoods(primary_emotion)
+      this.setState({ primary: primary_emotion })
     }
-    
+
 
   }
-  
+
   render() {
-    
+
     return (<View>
       <HeaderHome></HeaderHome>
       {
-        
-        this.state.moods.map((item,index)=>{
+
+        this.state.moods.map((item, index) => {
           console.log(item)
           let button;
-    if (this.state.moods[0].tertiary_emotion){
-     button = <FeelingButton class={item.tertiary_emotion} onPress={(e) => this.handlePress(e)} key={item.tertiary_emotion} emotion={item.tertiary_emotion}></FeelingButton>
-    }else if (this.state.moods[0].secondary_emotion){
-     button = <FeelingButton class={item.secondary_emotion}  onPress={(e) => this.handlePress(e, item.primary_emotion, item.secondary_emotion)} key={item.secondary_emotion} emotion={item.secondary_emotion}></FeelingButton>
-    }else{
-     button = <FeelingButton class={item.primary_emotion} onPress={(e) => this.handlePress(e,item.primary_emotion)} key={item.primary_emotion} emotion={item.primary_emotion}></FeelingButton> 
-    }
-          return button})
+          if (this.state.moods[0].tertiary_emotion) {
+            button = <FeelingButton class={item.tertiary_emotion} onPress={(e) => this.handlePress(e)} key={item.tertiary_emotion} emotion={item.tertiary_emotion}></FeelingButton>
+          } else if (this.state.moods[0].secondary_emotion) {
+            button = <FeelingButton class={item.secondary_emotion} onPress={(e) => this.handlePress(e, this.state.primary, item.secondary_emotion)} key={item.secondary_emotion} emotion={item.secondary_emotion}></FeelingButton>
+          } else {
+            button = <FeelingButton class={item.primary_emotion} onPress={(e) => this.handlePress(e, item.primary_emotion)} key={item.primary_emotion} emotion={item.primary_emotion}></FeelingButton>
+          }
+          return button
+        })
       }
-      </View>
+    </View>
     )
   }
 }

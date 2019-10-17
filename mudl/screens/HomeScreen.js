53b4ea9e77d64ed02eef95e43f5f422b4a6fe804
Zac Morton <zmorton8057@gmail.com>
2019-10-16 20:41:31 -0700

    first button working

--- mudl/screens/HomeScreen.js
+++ mudl/screens/HomeScreen.js
@@ -26,8 +26,10 @@ class HomeScreen extends Component {
       level: 'primary_emotion'
     }
   }
-  componentDidMount() {
-    API.getMoods()
+  updateMoods(primary_emotion,secondary_emotion){
+    let e1=primary_emotion||null;
+    let e2=secondary_emotion||null;
+    API.getMoods(e1,e2)
       .then(resp => {
         this.setState({ moods: resp.data })
         console.log("log during component did mount")
@@ -37,8 +39,15 @@ class HomeScreen extends Component {
         console.error(err)
       })
   }
-  handlePress(e, item){
-    
+  componentDidMount() {
+    this.updateMoods()
+  }
+  handlePress(e, primary_emotion, secondary_emotion){
+    if(primary_emotion && secondary_emotion){
+      this.updateMoods(primary_emotion,secondary_emotion)
+    }else if(primary_emotion){
+      this.updateMoods(primary_emotion)
+    }
     
 
   }
@@ -53,9 +62,9 @@ class HomeScreen extends Component {
           console.log(item)
           let button;
     if (this.state.moods[0].tertiary_emotion){
-     button = <FeelingButton class={item.tertiary_emotion} onPress={(e) => this.handlePress(e, item.tertiary_emotion)} key={item.tertiary_emotion} emotion={item.tertiary_emotion}></FeelingButton>
+     button = <FeelingButton class={item.tertiary_emotion} onPress={(e) => this.handlePress(e)} key={item.tertiary_emotion} emotion={item.tertiary_emotion}></FeelingButton>
     }else if (this.state.moods[0].secondary_emotion){
-     button = <FeelingButton class={item.secondary_emotion}  onPress={(e) => this.handlePress(e, item.secondary_emotion)} key={item.secondary_emotion} emotion={item.secondary_emotion}></FeelingButton>
+     button = <FeelingButton class={item.secondary_emotion}  onPress={(e) => this.handlePress(e, item.primary_emotion, item.secondary_emotion)} key={item.secondary_emotion} emotion={item.secondary_emotion}></FeelingButton>
     }else{
      button = <FeelingButton class={item.primary_emotion} onPress={(e) => this.handlePress(e,item.primary_emotion)} key={item.primary_emotion} emotion={item.primary_emotion}></FeelingButton> 
     }

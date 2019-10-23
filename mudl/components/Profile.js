b50a9f7945818566308e6dfba1b6232052adfa88
Zac Morton <zmorton8057@gmail.com>
2019-10-23 15:57:25 -0700

    Added Pie Chart to user profile page

--- mudl/components/Profile.js
+++ mudl/components/Profile.js
@@ -4,46 +4,113 @@ import {
   Text,
   View,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
+  Dimensions
 } from 'react-native';
-import {Ionicons} from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from 'react-native-chart-kit'
+
 
 const Profile = props => {
-    return (
-      <View style={styles.container}>
-          <View style={styles.header}></View>
-          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
-          <View style={styles.body}>
-          
-            <View style={styles.bodyContent}>
-            <Text style={styles.name}>{props.user}</Text>
-              <Text style={styles.info}>UX Designer / Mobile developer</Text>
-              <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
-              
-              <Text>Angry: {props.data.angry}</Text>
-              <Text>Disgusted: {props.data.disgusted}</Text>
-              <Text>Sad: {props.data.sad}</Text>
-              <Text>Happy: {props.data.happy}</Text>
-              <Text>Surprised: {props.data.surprised}</Text>
-              <Text>Fearful: {props.data.fearful}</Text>
-              <TouchableOpacity onPress={props.press} style={styles.buttonContainer}>
-                 <Ionicons name={"md-refresh"}></Ionicons>
-              </TouchableOpacity>              
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Opcion 2</Text> 
-              </TouchableOpacity>
-            </View>
+  return (
+
+    <View style={styles.container}>
+      <View style={styles.header}></View>
+      <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
+      <View style={styles.body}>
+
+        <View style={styles.bodyContent}>
+          <Text style={styles.name}>{props.user}</Text>
+          <Text style={styles.info}>30 Day Primary Emotion Log</Text>
+          <TouchableOpacity onPress={props.press} >
+            <Ionicons style={styles.refresh} size={32} name={"md-refresh"}></Ionicons>
+          </TouchableOpacity>
+
+          <PieChart
+
+            data={[
+              {
+                name: 'Angry',
+                population: props.data.angry,
+                color: '#A3E7FC',
+                legendFontColor: '#00232d',
+                legendFontSize: 15,
+              },
+              {
+                name: 'Disgusted',
+                population: props.data.disgusted,
+                color: '#259FC4',
+                legendFontColor: '#00232d',
+                legendFontSize: 15,
+              },
+              {
+                name: 'Sad',
+                population: props.data.sad,
+                color: '#31798E',
+                legendFontColor: '#00232d',
+                legendFontSize: 15,
+              },
+              {
+                name: 'Happy',
+                population: props.data.happy,
+                color: '#394D54',
+                legendFontColor: '#00232d',
+                legendFontSize: 15,
+              },
+              {
+                name: 'Surprised',
+                population: props.data.surprised,
+                color: '#00232d',
+                legendFontColor: '#00232d',
+                legendFontSize: 15,
+              },
+              {
+                name: 'Fearful',
+                population: props.data.fearful,
+                color: '#90CBDD',
+                legendFontColor: '#00232d',
+                legendFontSize: 15,
+              },
+            ]}
+            width={Dimensions.get("window").width}
+            height={220}
+            chartConfig={{
+              backgroundColor: '#1cc910',
+              backgroundGradientFrom: '#eff3ff',
+              backgroundGradientTo: '#efefef',
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+              style: {
+                borderRadius: 20,
+              },
+            }}
+            style={{
+              marginVertical: 8,
+              borderRadius: 16,
+            }}
+            accessor="population"
+            backgroundColor="transparent"
+            paddingLeft="15"
+          />
         </View>
       </View>
-    );
-  }
+    </View>
+  );
+}
 
 
 const styles = StyleSheet.create({
-  header:{
-    backgroundColor: "#1ebbd0",
-    height:100,
-    borderBottomColor:'#51A3A3',
+  header: {
+    backgroundColor: "#00232d",
+    height: 100,
+    borderBottomColor: '#51A3A3',
     borderBottomWidth: 15
   },
   avatar: {
@@ -51,56 +118,44 @@ const styles = StyleSheet.create({
     height: 115,
     borderRadius: 63,
     borderWidth: 4,
-    borderColor: "white",
-    marginBottom:10,
-    alignSelf:'flex-start',
+    borderColor: "#51A3A3",
+    marginBottom: 10,
+    alignSelf: 'center',
     position: 'absolute',
-    marginTop:45,
-    marginLeft:30
+    marginTop: 45,
   },
-  name:{
-    fontSize:22,
-    color:"#1ebbd0",
-    fontWeight:'600',
-    zIndex:1
+  name: {
+    fontSize: 22,
+    color: "#00232d",
+    fontWeight: '600',
+    zIndex: 1
   },
-  body:{
-    marginTop:40,
-    
+  body: {
+    marginTop: 40,
   },
   bodyContent: {
     alignItems: 'center',
-    padding:30,
+    padding: 30,
   },
-  name:{
-    fontSize:28,
-    color: "#1ebbd0",
+  name: {
+    fontSize: 28,
+    color: "#00232d",
     fontWeight: "600"
   },
-  info:{
-    fontSize:16,
-    color: "#1ebbd0",
-    marginTop:10
+  info: {
+    fontSize: 16,
+    color: "#00232d",
+    marginTop: 10
   },
-  description:{
-    fontSize:16,
-    color: "#696969",
-    marginTop:10,
+  description: {
+    fontSize: 16,
+    color: "#00232d",
+    marginTop: 10,
     textAlign: 'center'
   },
-  buttonContainer: {
-    marginTop:10,
-    height:45,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
-    backgroundColor: "#1ebbd0",
-    borderColor:'#51A3A3',
-    borderWidth: 3
-  },
+  refresh: {
+    textAlign: 'right'
+  }
 });
 
 export default Profile;
